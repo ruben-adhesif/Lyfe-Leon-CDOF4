ae14399c6fca445138da873abf65f2edd4d1cf89
Henri Serano <srn.henri@gmail.com>
2024-02-05 11:57:25 +0100

    fix a bug

--- Lyfe.py
+++ Lyfe.py
@@ -6,6 +6,7 @@ LOOP_LENGHT = 600
 #You can fix this bug also by changing the variable name to LOOP_LENGTH 
 plt.rcParams["animation.html"] = 'jshtml'
 
+#I see another bug also in the code, the variable name is LOOP_LENGHT, but it should be LOOP_LENGTH
 board = sg.Board(size=(30,30))
 board.add(lf.Blinker(length=3), loc=(4,4))
 board.add(lf.Glider(), loc=(10,4))
